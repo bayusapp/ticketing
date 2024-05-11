bf78fc3e5009bf0d209dac8444c3222ff4baf79e
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-11 16:01:34 +0700

    add auto routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,32 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+// Default Routes from CodeIgniter 4.5.1
+// use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
+// /**
+//  * @var RouteCollection $routes
+//  */
+// $routes->get('/', 'Home::index');
+
+namespace Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/*
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
  */
-$routes->get('/', 'Home::index');
+// $routes->setDefaultNamespace('App\Controllers');
+// $routes->setDefaultController('Home');
+// $routes->setDefaultMethod('index');
+// $routes->setTranslateURIDashes(false);
+// $routes->set404Override();
+$routes->setAutoRoute(true);
